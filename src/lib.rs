e7a9489187757a86a4dc7baee82f2f216d5839f4
christopher k <30932534+EleisonC@users.noreply.github.com>
2025-03-11 23:41:29 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,4 @@ mod flux_memory;
 mod flux_scheduler;
 mod krono_task_queue;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
